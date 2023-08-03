336ab09b43ee83349c7eea960b0c2e1e77962690
Sai Barath <saibarathdeep@outlook.com>
2023-08-04 02:47:27 +0530

    added function to get images from mapbox

--- src/components/common/CustomMapBoxRenderer.jsx
+++ src/components/common/CustomMapBoxRenderer.jsx
@@ -4,12 +4,21 @@ import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2FpYmFyYXRociIsImEiOiJjbGt2OW92b2kwM2Y0M2pueGYwazVjazM3In0.lR7Be6FsuuWiomVLWaNP0Q';
 
-export default function CustomMapBoxRenderer(){
-    const mapContainer = useRef(null);    
+export default function CustomMapBoxRenderer({ renderImage }) {
+    const mapContainer = useRef(null);
     const map = useRef(null);
     const [lng, setLng] = useState(82.7553);
     const [lat, setLat] = useState(21.4542);
     const [zoom, setZoom] = useState(4.22);
+    const [bounds, setBounds] = useState(null);
+
+    const handleClickPrint = () => {
+        if (bounds) {
+            const coordinates = [lng, lat]
+            const imageUrl = `https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/${coordinates},${zoom},0/400x400?access_token=${mapboxgl.accessToken}`;
+            return imageUrl;
+        }
+    }
 
     useEffect(() => {
         if (map.current) return; // initialize map only once
@@ -23,6 +32,7 @@ export default function CustomMapBoxRenderer(){
             setLng(map.current.getCenter().lng.toFixed(4));
             setLat(map.current.getCenter().lat.toFixed(4));
             setZoom(map.current.getZoom().toFixed(2));
+            setBounds(map.current.getBounds());
         });
         const geocoder = new MapboxGeocoder({
             accessToken: mapboxgl.accessToken,
@@ -32,7 +42,9 @@ export default function CustomMapBoxRenderer(){
             mapboxgl: mapboxgl
         });
         map.current.addControl(geocoder);
+        map.current.addControl(new mapboxgl.NavigationControl());
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return(
